8db290d14214fc69b84368910d5ad08fb78014fd
Kelson Martins <kmartins@ie.ibm.com>
2020-11-18 21:50:54 +0000

    adding first run cobra command

---
+++ cmd/run.go
@@ -0,0 +1,170 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+
+	_ "github.com/mattn/go-sqlite3"	
+)
+
+var pomodoroMinute int = 1
+var gomodoroMinutes int = 25
+var gomodoroDB, _ = sql.Open("sqlite3", "./gomodoro.db")
+var startDatetime = time.Now()
+var startDate = startDatetime.Format("02-01-2006")
+var category string
+var subcategory string
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		
+		fmt.Println("run called")
+
+		defer gomodoroDB.Close()
+		initDB(gomodoroDB)
+	
+		startTime := startDatetime.Format("15:04:05")
+		fmt.Println("Pomodoro Started:", startDate, startTime)
+	
+		go pomodoroHeartbeat()
+		time.Sleep(25 * time.Second)
+	
+		finishPomodoro(startTime)		
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+	runCmd.PersistentFlags().StringVarP(&category, "category", "c", "", "gomodoro category (required)")
+	runCmd.MarkPersistentFlagRequired("category")
+}
+
+func pomodoroHeartbeat() {
+	ticker := time.NewTicker(time.Second)
+
+	for range ticker.C {
+		timeMod := pomodoroMinute%5 == 0
+		if timeMod == true && pomodoroMinute != 25 {
+			println("Remaining minutes:", gomodoroMinutes-pomodoroMinute)
+		}
+		pomodoroMinute++
+
+		if pomodoroMinute == 26 {
+			ticker.Stop()
+		}
+
+	}
+}
+
+func finishPomodoro(startTime string) {
+
+	endTime := startDatetime.Add(time.Minute * 25).Format("15:04:05")
+
+	fmt.Println("Pomodoro Finished:", startDate, endTime)
+
+	f, err := os.Open("./assets/tone.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+
+	insertRecord(gomodoroDB, startDate, startTime, endTime, gomodoroMinutes, category, "")
+
+	displayEnd()
+
+}
+
+func displayEnd() {
+	a := app.New()
+	w := a.NewWindow("Gomodoro")
+
+	text := widget.NewLabel("Gomodoro Finished")
+	w.SetContent(widget.NewVBox(
+		text,
+	))
+
+	w.ShowAndRun()
+}
+
+func initDB(db *sql.DB) {
+	createTable(db)
+}
+
+func createTable(db *sql.DB) {
+	createTableGomodoros := `CREATE TABLE IF NOT EXISTS gomodoros (
+		"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"date" DATE NOT NULL,		
+		"startTimestamp" TIME,
+		"endTimestamp" TIME,
+		"minutes" INT NOT NULL,
+		"category" TEXT NOT NULL,
+		"subCategory" TEXT
+	  );`
+
+	statement, err := db.Prepare(createTableGomodoros)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	statement.Exec()
+}
+
+func insertRecord(db *sql.DB, date string, startTimestamp string, endTimestamp string, minutes int, category string, subcategory string) {
+	insertGomodoroSQL := `INSERT INTO gomodoros(date, startTimestamp, endTimestamp, minutes, category, subCategory) VALUES (?,?,?,?,?,?)`
+	statement, err := db.Prepare(insertGomodoroSQL)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(date, startTimestamp, endTimestamp, minutes, category, subcategory)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
